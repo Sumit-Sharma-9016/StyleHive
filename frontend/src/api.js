de81f16b03ae422dbe8fd5a8758683252cde3db2
Sumit Sharma <sumit901693@gmail.com>
2025-08-11 01:17:52 +0530

    backend url update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/"
+    baseURL: "https://stylehive-backend-rbi0.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
